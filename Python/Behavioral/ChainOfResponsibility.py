d294c441c1ceb876104d8057663b7d8b092849c2
Caidan Williams <caidanstevenwilliams@gmail.com>
2019-02-24 17:45:35 -0700

    pythonic code refactor: chain of responsibility

--- Python/Behavioral/ChainOfResponsibility.py
+++ Python/Behavioral/ChainOfResponsibility.py
@@ -1,49 +1,52 @@
-import inspect
-
 class Account:
     _successor = None
     _balance = None
 
-    def setNext(self, account):
+    def set_next(self, account):
         self._successor = account
 
-    def pay(self, amountToPay):
-        import inspect
-        myCaller = inspect.stack()[1][3]
-        if self.canPay(amountToPay):
-            print "Paid " + str(amountToPay) + " using " + myCaller
-        elif (self._successor):
-            print "Cannot pay using " + myCaller + ". Proceeding .."
-            self._successor.pay(amountToPay)
+    def pay(self, amount_to_pay):
+        instance_class_name = self.__class__.__name__
+        if self.can_pay(amount_to_pay):
+            print('Paid $' + str(amount_to_pay) + ' using ' + instance_class_name)
+        elif self._successor:
+            print('Cannot pay using ' + instance_class_name + '...')
+            print('\t...checking next payment method')
+            self._successor.pay(amount_to_pay)
         else:
-            raise ValueError('None of the accounts have enough balance')
-    def canPay(self, amount):
-        return self.balance >= amount
+            raise ValueError('None of the accounts have enough _balance')
+
+    def can_pay(self, amount: int):
+        return self._balance >= amount
+
 
 class Bank(Account):
     _balance = None
 
     def __init__(self, balance):
-        self.balance = balance
+        self._balance = balance
+
 
 class Paypal(Account):
     _balance = None
 
     def __init__(self, balance):
-        self.balance = balance
+        self._balance = balance
+
 
 class Bitcoin(Account):
     _balance = None
 
     def __init__(self, balance):
-        self.balance = balance
+        self._balance = balance
+
 
 if __name__ == '__main__':
     bank = Bank(100)
     paypal = Paypal(200)
     bitcoin = Bitcoin(300)
 
-    bank.setNext(paypal)
-    paypal.setNext(bitcoin)
+    bank.set_next(paypal)
+    paypal.set_next(bitcoin)
 
     bank.pay(259)
